248cb4a5223803aa0606f9eec6a5fd8ce33d8759
Ulrich Germann <ulrich.germann@gmail.com>
2018-09-21 16:31:03 +0100

    CMD available with raw engine. More flexible model download script.

--- engine/download_models.sh
+++ engine/download_models.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+set -e
 
-#rm -rf model
-mkdir -p model
+MODEL_URL=$1
+path=$2
 
-cd model
+mkdir -p $path
+
+cd $path
 [ ! -f model.zip ] && [ "$MODEL_URL" != "" ] && wget "${MODEL_URL}" -O model.zip
 [ -f model.zip ] && unzip -n model.zip
 
 # Backward compatibility with old models
 find . -name "*cmvn*" | xargs -I {} sed -i "s/--norm-mean=/--norm-means=/" {}
+
+# fix an error in v3 of the arabic models
+sed -i 's/^-1-/--/' alex_asr.conf
