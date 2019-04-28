39dd01be5af8321b773a310225c627f59e2ddbcc
Ulrich Germann <ugermann@inf.ed.ac.uk>
2019-04-28 01:40:01 +0100

    Update transcribe.sh

--- transcribe.sh
+++ transcribe.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 mydir=$(dirname $(realpath $0))
-docker run --rm -it \
+docker run --rm -i \
        -v ${mydir}/engine:/engine \
        -v $1:/model \
-       -v $2:/audio.wav summaplatform/asr-engine \
-       /engine/transcribe.py -m /model /audio.wav
+       -v $2:/$(basename $2) summaplatform/asr-engine \
+       /engine/transcribe.py -m /model /$(basename $2) 
 
 #       /engine/test_transcriber.py /model /audio.wav
